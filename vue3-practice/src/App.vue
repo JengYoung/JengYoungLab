136e0622cffaec896ab0157357ad6e26de70c42b
Jengyoung <young_pallete@naver.com>
2021-10-06 19:26:36 +0900

    feat: vue3 boilerplate

---
+++ vue3-practice/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1 v-if="msg" class="message" @click="log">
+    {{ msg }}
+  </h1>
+  <Hello />
+</template>
+
+<script>
+import Hello from '@/components/Hello';
+export default {
+  components: {
+    Hello,
+  },
+  data() {
+    return {
+      msg: '왜???',
+    };
+  },
+  methods: {
+    log() {
+      window.addEventListener();
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+h1 {
+  color: red;
+}
+</style>
