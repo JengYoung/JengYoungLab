6c75902e31378fbced15d8a70228041cb4f537ed
Jengyoung <young_pallete@naver.com>
2021-12-02 23:34:03 +0900

    feat: todo context added

---
+++ nextjs-axios-api-test/contexts/todo.js
@@ -0,0 +1,25 @@
+import React, { createContext, useCallback, useContext, useState } from "react";
+
+const initialState = [];
+
+const TodosContext = createContext();
+
+export const useTodos = () => useContext(todoContext);
+
+const TodosProvider = ({ children }) => {
+  const [todos, setTodos] = useState(initialState);
+
+  const addTodo = useCallback((todo) => {
+    setTodos((state) => [...state, todo]);
+  }, []);
+
+  const removeTodo = useCallback((todo) => {
+    setTodos((state) => state.filter((stateTodo) => stateTodo.id !== todo.id));
+  }, []);
+
+  return (
+    <TodosContext.provider value={{ todos, addTodo, removeTodo }}>
+      {children}
+    </TodosContext.provider>
+  );
+};
