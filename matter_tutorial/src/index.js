88bca290e688c093e9e0d785661aeb9ff41a587b
Jengyoung <young_pallete@naver.com>
2021-12-25 22:57:36 +0900

    feat: add star svg
    - 커밋 설명:
      기존 별을 그리는 데 필요한 `star` svg를 추가하였다.
    
    - 체크 리스트:
    - [x] 3초마다 계속해서 도형을 50개씩 렌더링한다.
    - [x] 렌더할 때마다 엔진의 무중력 상태가 구현된다.
    - [x] 밤 하늘 및 별의 색깔을 구현한다.
    - [x] 밤 하늘에 별을 렌더링한다.

--- matter_tutorial/src/index.js
+++ matter_tutorial/src/index.js
@@ -1,17 +1,15 @@
 import Matter from 'matter-js';
-import star from './Star.svg';
-
-console.log(star);
 // module aliases
 const Engine = Matter.Engine;
 const Render = Matter.Render;
 const Runner = Matter.Runner;
 const Bodies = Matter.Bodies;
 const Composite = Matter.Composite;
+const Vertices = Matter.Vertices;
 
 // create an engine
 const engine = Engine.create();
-engine.gravity.y = -0.2;
+engine.gravity.y = -0.1;
 
 // create a renderer
 const render = Render.create({
@@ -25,12 +23,17 @@ const render = Render.create({
   },
 });
 
+const star = Vertices.fromPath(
+  '50 0 63 38 100 38 69 59 82 100 50 75 18 100 31 59 0 38 37 38',
+);
+
+Matter.Vertices.scale(star, -0.2, -0.2);
 const circles = [];
 // create two boxes and a ground
 for (let i = 0; i < 55; i += 1) {
   circles.push(
-    Bodies.circle(10 + i * 10, 400 + Math.random(100) * 5000, 8, {
-      render: { fillStyle: '#fee433' },
+    Bodies.fromVertices(10 + i * 10, 400 + Math.random(100) * 5000, star, {
+      render: { fillStyle: '#fee433', lineWidth: 1, strokeStyle: '#fee433' },
     }),
   );
 }
@@ -46,15 +49,20 @@ const runner = Runner.create();
 // run the engine
 Runner.run(runner, engine);
 
-setInterval(() => {
-  const circles = [];
-  for (let i = 0; i < 55; i += 1) {
-    circles.push(
-      Bodies.circle(10 + i * 10, 400 + Math.random(100) * 5000, 10, {
-        render: { fillStyle: '#5162ff' },
-      }),
-    );
-  }
-  console.log('hihi');
-  Composite.add(engine.world, [...circles]);
-}, 15000);
+// setInterval(() => {
+//   const circles = [];
+//   for (let i = 0; i < 55; i += 1) {
+//     circles.push(
+//       Bodies.fromVertices(
+//         10 + i * 10,
+//         400 + Math.random(100) * 5000,
+//         starPaths,
+//         {
+//           render: { fillStyle: '#5162ff' },
+//         },
+//       ),
+//     );
+//   }
+//   console.log('hihi');
+//   Composite.add(engine.world, [...circles]);
+// }, 15000);
