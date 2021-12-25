afd1bbefab6f4ab7e7b9a477d2727d088304a9ed
Jengyoung <young_pallete@naver.com>
2021-12-25 15:44:32 +0900

    chore: yarn berry setting / feat: first matterjs
    1. `yarn berry`를 통한 의존성 및 종속성 관리
    2. matterjs를 실행하기 위한 기본 세팅 추가
    3. matterjs 기본 코드 작성 및 구현
    
    해당 구현 과정은 다음 문서로 기술하였습니다.
    https://nonstop-fender-e38.notion.site/yarn-berry-setting-ba448596d48e402cb043c3fe3eea40a5
    https://www.notion.so/matterjs-d4526bad603f4d06a9d3fdb4f91b8bee

---
+++ matter_tutorial/src/index.js
@@ -0,0 +1,34 @@
+import Matter from "matter-js";
+
+// module aliases
+const Engine = Matter.Engine,
+  Render = Matter.Render,
+  Runner = Matter.Runner,
+  Bodies = Matter.Bodies,
+  Composite = Matter.Composite;
+
+// create an engine
+const engine = Engine.create();
+
+// create a renderer
+const render = Render.create({
+  element: document.body,
+  engine: engine,
+});
+
+// create two boxes and a ground
+const boxA = Bodies.rectangle(400, 200, 80, 80);
+const boxB = Bodies.rectangle(450, 50, 80, 80);
+const ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+// add all of the bodies to the world
+Composite.add(engine.world, [boxA, boxB, ground]);
+
+// run the renderer
+Render.run(render);
+
+// create runner
+const runner = Runner.create();
+
+// run the engine
+Runner.run(runner, engine);
