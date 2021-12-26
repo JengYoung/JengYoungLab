805a9544c31a4dfe33af3757200cad1ac30094c4
Jengyoung <young_pallete@naver.com>
2021-12-26 14:14:15 +0900

    feat: matterjs을 통한 2D 물리 엔진 시뮬레이션 최종 구현
    - 커밋 설명:
      현재 이름에 닿으면 도형들이 튕겨져 나가는 2D 시뮬레이션을 구현했다.
    
    - 체크 리스트:
      - [x] 화면 상으로 사람들에게 인터렉티브한 효과를 배가하기 위해 색상을 조절한다.
      - [x] 화면 상에서 과부하가 걸리지 않을만큼의 컴포넌트 개수를 설정하여 최적화를 진행한다.
      - [x] 여러 도형들은 부딪히면 튕겨져 나간다.
      - [x] 글자는 고정되어 있는 채로 있는다.

--- matter_tutorial/src/index.js
+++ matter_tutorial/src/index.js
@@ -4,21 +4,38 @@ const Engine = Matter.Engine;
 const Render = Matter.Render;
 const Runner = Matter.Runner;
 const Bodies = Matter.Bodies;
+const World = Matter.World;
 const Composite = Matter.Composite;
 const Vertices = Matter.Vertices;
 
+const font = [];
+const letters = document.getElementById('jengyoung')?.querySelectorAll('path');
+letters.forEach(path => {
+  font.push(Matter.Svg.pathToVertices(path));
+});
+
+Matter.Vertices.scale(font, 1.5, 1.5);
 // create an engine
 const engine = Engine.create();
-engine.gravity.y = -0.1;
+engine.gravity.y = -0.5;
+
+World.add(
+  engine.world,
+  Bodies.fromVertices(500, 120, font, {
+    isStatic: true,
+    render: { fillStyle: '#fafafa', strokeStyle: '#fafafa', lineWidth: 0 },
+    wireframes: true,
+  }),
+);
 
 // create a renderer
 const render = Render.create({
   element: document.body,
   engine,
   options: {
-    width: 560,
-    height: 400,
-    background: 'linear-gradient(#2f1347, #161611)',
+    width: 940,
+    height: 480,
+    background: 'linear-gradient(#03001e, #7303c0)',
     wireframes: false,
   },
 });
@@ -27,12 +44,27 @@ const star = Vertices.fromPath(
   '50 0 63 38 100 38 69 59 82 100 50 75 18 100 31 59 0 38 37 38',
 );
 
-Matter.Vertices.scale(star, -0.2, -0.2);
+Matter.Vertices.scale(star, -0.1875, -0.1875);
 const circles = [];
 // create two boxes and a ground
-for (let i = 0; i < 55; i += 1) {
+for (let i = 0; i < 100; i += 1) {
   circles.push(
-    Bodies.fromVertices(10 + i * 10, 400 + Math.random(100) * 5000, star, {
+    Bodies.circle(10 + i * 10, 500 + Math.random(100) * 2500, 4, {
+      render: { fillStyle: '#F9C2EA', lineWidth: 1, strokeStyle: '#F9C2EA' },
+    }),
+    Bodies.circle(10 + i * 10, 500 + Math.random(100) * 2500, 4, {
+      render: { fillStyle: '#F16FCE', lineWidth: 1, strokeStyle: '#F16FCE' },
+    }),
+    Bodies.circle(10 + i * 10, 500 + Math.random(100) * 2500, 4, {
+      render: { fillStyle: '#F9C2EA', lineWidth: 1, strokeStyle: '#F9C2EA' },
+    }),
+    Bodies.circle(10 + i * 10, 500 + Math.random(100) * 2500, 4, {
+      render: { fillStyle: '#F16FCE', lineWidth: 1, strokeStyle: '#F16FCE' },
+    }),
+    Bodies.circle(10 + i * 10, 500 + Math.random(100) * 2500, 4, {
+      render: { fillStyle: '#B620BE', lineWidth: 1, strokeStyle: '#B620BE' },
+    }),
+    Bodies.fromVertices(10 + i * 10, 500 + Math.random(100) * 2500, star, {
       render: { fillStyle: '#fee433', lineWidth: 1, strokeStyle: '#fee433' },
     }),
   );
@@ -51,18 +83,18 @@ Runner.run(runner, engine);
 
 // setInterval(() => {
 //   const circles = [];
-//   for (let i = 0; i < 55; i += 1) {
+//   for (let i = 0; i < 100; i += 1) {
 //     circles.push(
-//       Bodies.fromVertices(
-//         10 + i * 10,
-//         400 + Math.random(100) * 5000,
-//         starPaths,
-//         {
-//           render: { fillStyle: '#5162ff' },
-//         },
-//       ),
+//       Bodies.fromVertices(10 + i * 10, 500 + Math.random(100) * 5000, star, {
+//         render: { fillStyle: '#fee433', lineWidth: 1, strokeStyle: '#fee433' },
+//       }),
+//       Bodies.circle(10 + i * 10, 500 + Math.random(100) * 5000, 4, {
+//         render: { fillStyle: '#fee433', lineWidth: 1, strokeStyle: '#fee433' },
+//       }),
+//       Bodies.rectangle(10 + i * 10, 500 + Math.random(100) * 5000, 12, 12, {
+//         render: { fillStyle: '#fee433', lineWidth: 1, strokeStyle: '#fee433' },
+//       }),
 //     );
 //   }
-//   console.log('hihi');
 //   Composite.add(engine.world, [...circles]);
 // }, 15000);
