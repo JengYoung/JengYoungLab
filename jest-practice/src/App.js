33a077e6267d9f9aabefa9e4c62c4887e2873fc7
Jengyoung <young_pallete@naver.com>
2022-01-01 23:56:32 +0900

    test: Basic test with jest framework
    * 커밋 내용
    1. `getByText`를 통한 기본 텍스트 테스트 진행 완료
    2. `getByRole`을 통한 이미지 경로 및 `alt` 속성 체크 테스트 완료
    
    * 참고 자료
    현재 테스트 내용을 다음 TIL 아티클로 기록하였습니다. 링크를 참고해주세요.
    https://nonstop-fender-e38.notion.site/CRA-Jest-2aa8fd271da04c92adc6a1dd0aee1288

--- jest-practice/src/App.js
+++ jest-practice/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Logo from "./Logo";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Logo></Logo>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- jest-practice/src/App.test.js
+++ jest-practice/src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import Logo from "./Logo";
+import logoSvg from "./logo.svg";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+describe("<Logo />", () => {
+  it("renders an image", () => {
+    render(<Logo />);
+    const logo = screen.getByRole("img");
+    expect(logo).toHaveAttribute("src", logoSvg);
+    expect(logo).toHaveAttribute("alt", "logo");
+  });
 });
