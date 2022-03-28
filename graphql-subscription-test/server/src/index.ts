252c0677f94fbefbe98ebf8d14e7a576bd3e83b0
JengYoung <young_pallete@naver.com>
2022-03-28 16:26:10 +0900

    :wrench: add typescript eslint to operate linter
    eslint가 타입스크립트 환경에서 제대로 작동하도록 설정

--- graphql-subscription-test/server/src/index.ts
+++ graphql-subscription-test/server/src/index.ts
@@ -1,2 +1,26 @@
 import { config as dotenvConfig } from 'dotenv';
 dotenvConfig();
+
+import express from 'express';
+import { createServer } from 'http';
+import cors from 'cors';
+
+const PORT = process.env.BASE_PORT;
+const message: string = 'Hello!';
+
+(async () => {
+  const app = express();
+  const server = createServer(app);
+  const corsOptions = {
+    origin: '*',
+  };
+  app.use(cors(corsOptions));
+
+  app.get('/', (req, res) => {
+    res.send('Hello!');
+  });
+
+  server.listen(PORT, () => {
+    console.log(`listening to port ${PORT}...`);
+  });
+})();
