676bbc2376e1543befd3442469847cdda0b96d0d
JengYoung <young_pallete@naver.com>
2022-05-21 20:12:33 +0900

    :sparkles: 페이지 트랜지션 효과 추가

--- typeform-practice/src/App.vue
+++ typeform-practice/src/App.vue
@@ -3,8 +3,29 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view />
+  <router-view v-slot="{ Component }">
+    <transition name="fade-transform" mode="out-in">
+      <keep-alive :include="[HomeView, AboutView]">
+        <component :is="Component" />
+      </keep-alive>
+    </transition>
+  </router-view>
 </template>
+
+<script lang="ts">
+import HomeView from "@/views/HomeView.vue";
+import AboutView from "@/views/AboutView.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {
+      HomeView,
+      AboutView,
+    };
+  },
+});
+</script>
 
 <style lang="scss">
 #app {
@@ -26,5 +47,35 @@ nav {
       color: #42b983;
     }
   }
+}
+
+/* Global transition */
+// See https://vuejs.org/v2/guide/transitions.html for detail
+
+// fade
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.28s;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
+
+// fade-transform
+.fade-transform-leave-active,
+.fade-transform-enter-active {
+  transition: all 0.5s;
+}
+
+.fade-transform-enter-from {
+  opacity: 0;
+  transform: translateX(-30px);
+}
+
+.fade-transform-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
 }
 </style>
