b36b984b3e95d623cc8d6f6db86b9f945bc0dd34
JengYoung <young_pallete@naver.com>
2022-05-23 23:33:31 +0900

    :sparkles: add todo-page component layout

--- blog-practice-for-junior/src/views/TodoPage.vue
+++ blog-practice-for-junior/src/views/TodoPage.vue
@@ -1,19 +1,30 @@
 <template>
-  <div>TODO Page</div>
+  <header>
+    <h1>TODO PAGE</h1>
+  </header>
+  <form class="form" action="">
+    <input class="form__input" type="text" placeholder="할 일을 입력하세요!" />
+    <button class="form__submit-btn">입력</button>
+  </form>
+  <ul class="todos"></ul>
 </template>
 
 <script lang="ts">
-import { getPosts } from '@/api/post';
-import { defineComponent, onMounted } from 'vue';
+import { getPosts } from '../api/post';
+import { defineComponent, onMounted, ref } from 'vue';
 export default defineComponent({
   setup() {
+    const todos = ref([]);
+
     onMounted(() => {
       (async () => {
         const data = await getPosts();
         console.log('data: ', data);
       })();
     });
-    return {};
+    return {
+      todos,
+    };
   },
 });
 </script>
