6a7a9efdce47ec34f69c55a9c3ac4be1c25757d6
JengYoung <young_pallete@naver.com>
2022-05-23 23:48:53 +0900

    :sparkles: Todo-list 생성, 삭제 기능 구현

--- blog-practice-for-junior/src/views/TodoPage.vue
+++ blog-practice-for-junior/src/views/TodoPage.vue
@@ -2,11 +2,31 @@
   <header>
     <h1>TODO PAGE</h1>
   </header>
+
   <form class="form" action="">
-    <input class="form__input" type="text" placeholder="할 일을 입력하세요!" />
-    <button class="form__submit-btn">입력</button>
+    <input
+      class="form__input"
+      type="text"
+      placeholder="할 일을 입력하세요!"
+      v-model="inputValue"
+    />
+    <button class="form__submit-btn" @click.prevent="addTodo">입력</button>
   </form>
-  <ul class="todos"></ul>
+
+  <ul class="todos">
+    <li v-for="todo in todos" class="todo" :key="todo.id">
+      <span
+        class="todo__content"
+        :class="todo.completed ? 'todo__content--toggled' : ''"
+        @click="() => onToggle(todo.id)"
+      >
+        {{ todo.content }}
+      </span>
+      <button class="todo__remove-btn" @click="() => onRemove(todo.id)">
+        X
+      </button>
+    </li>
+  </ul>
 </template>
 
 <script lang="ts">
@@ -14,7 +34,32 @@ import { getPosts } from '../api/post';
 import { defineComponent, onMounted, ref } from 'vue';
 export default defineComponent({
   setup() {
-    const todos = ref([]);
+    const todos = ref<{ id: string; content: string; completed: boolean }[]>(
+      []
+    );
+    const inputValue = ref('');
+
+    const addTodo = () => {
+      todos.value = [
+        ...todos.value,
+        {
+          id: new Date().toString(),
+          content: inputValue.value,
+          completed: false,
+        },
+      ];
+      inputValue.value = '';
+    };
+
+    const onToggle = (id: string) => {
+      todos.value = todos.value.map((val) =>
+        val.id === id ? { ...val, completed: !val.completed } : val
+      );
+    };
+
+    const onRemove = (id: string) => {
+      todos.value = todos.value.filter((val) => val.id !== id);
+    };
 
     onMounted(() => {
       (async () => {
@@ -24,9 +69,20 @@ export default defineComponent({
     });
     return {
       todos,
+      addTodo,
+      inputValue,
+      onToggle,
+      onRemove,
     };
   },
 });
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.todo {
+  list-style: none;
+}
+.todo__content--toggled {
+  text-decoration: line-through;
+}
+</style>
