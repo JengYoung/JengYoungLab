2eab078a213297545ca0b7c46c3467b8c0525085
JengYoung <young_pallete@naver.com>
2022-05-24 00:12:27 +0900

    :lipstick: PostPage layout 구현

--- blog-practice-for-junior/src/views/PostPage.vue
+++ blog-practice-for-junior/src/views/PostPage.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>POST Page</div>
-  <div>{{ posts }}</div>
+  <header>
+    <h1>POST PAGE</h1>
+  </header>
+  <ul class="posts">
+    <li class="post" v-for="post in posts" :key="post.id">
+      <h3 class="post__title">{{ post.title }}</h3>
+      <div class="post__body">content: {{ post.body }}</div>
+      <div class="post__id">{{ post.id }}</div>
+    </li>
+  </ul>
 </template>
 
 <script lang="ts">
@@ -21,4 +29,26 @@ export default defineComponent({
 });
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.post {
+  cursor: pointer;
+  list-style: none;
+  text-align: left;
+
+  margin-bottom: 1rem;
+  padding: 1rem;
+
+  border: 1px solid lightgray;
+  border-radius: 20px;
+
+  &:hover {
+    background: #e5def2;
+    transition: all 0.3s;
+  }
+
+  .post__id {
+    text-align: right;
+    font-size: 0.75rem;
+  }
+}
+</style>
