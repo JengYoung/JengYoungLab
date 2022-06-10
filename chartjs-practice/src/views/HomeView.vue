28d768757d55bd011245a50fba27f6c9e0dd09ef
JengYoung <young_pallete@naver.com>
2022-06-10 23:59:04 +0900

    :sparkles: area chart example

--- chartjs-practice/src/views/HomeView.vue
+++ chartjs-practice/src/views/HomeView.vue
@@ -1,18 +1,47 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="about">
+    <canvas ref="chart" id="myChart" width="400" height="400"></canvas>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import {
+  defineComponent, onMounted, ref,
+} from 'vue';
+import { Chart, registerables } from 'chart.js';
+
+Chart.register(...registerables);
 
 export default defineComponent({
   name: 'HomeView',
-  components: {
-    HelloWorld,
+  setup() {
+    const chart = ref<HTMLCanvasElement | null>(null);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const myChart = ref<any>(null);
+    onMounted(() => {
+      const drawChart = async () => {
+        chart.value = (document.getElementById('myChart') as HTMLCanvasElement);
+
+        myChart.value = new Chart(chart.value as HTMLCanvasElement, {
+          data: {
+            datasets: [
+              {
+                fill: {
+                  target: 'origin',
+                  above: 'rgb(255, 0, 0)', // Area will be red above the origin
+                  below: 'rgb(0, 0, 255)', // And blue below the origin
+                },
+              },
+            ],
+          },
+        });
+      };
+      drawChart();
+    });
+
+    return {
+      myChart,
+    };
   },
 });
 </script>
