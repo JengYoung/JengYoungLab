ea82aecc0ee6095be839f0ec2e610d39ae343356
JengYoung <young_pallete@naver.com>
2022-07-24 16:39:38 +0900

    :sparkles: add Text component to application

--- monorepo-cra/jengyoungs/app/src/App.js
+++ monorepo-cra/jengyoungs/app/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { Text } from '@jengyoungs/common';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Text></Text>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
